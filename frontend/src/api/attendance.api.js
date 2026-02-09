6a000f62fa7662604a96d4f933683616aea5a874
Olapu Ajay <olapuajay@gmail.com>
2026-02-09 09:17:53 +0530

    Completed the backend

--- frontend/src/api/attendance.api.js
+++
@@ -1,6 +0,0 @@
-import api from "./axios";
-
-export const clockInApi = () => api.post("/attendance/clock-in");
-export const clockOutApi = () => api.post("/attendance/clock-out");
-export const getMyAttendanceApi = () => api.get("/attendance/me");
-export const getCompanyAttendanceApi = () => api.get("/attendance");
